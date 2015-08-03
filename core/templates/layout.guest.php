24c37dd9e83d773f06e57b004d6c337f29d99b47
Morris Jobke <hey@morrisjobke.de>
2015-08-03 18:24:20 +0200

    Add favicon for IE up to version 10

--- core/templates/layout.guest.php
+++ core/templates/layout.guest.php
@@ -11,6 +11,7 @@
 		<meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0">
 		<meta name="apple-itunes-app" content="app-id=<?php p($theme->getiTunesAppId()); ?>">
 		<meta name="theme-color" content="<?php p($theme->getMailHeaderColor()); ?>">
+		<link rel="icon" type="image/vnd.microsoft.icon" href="<?php print_unescaped(image_path('', 'favicon.ico')); ?>">
 		<link rel="shortcut icon" type="image/png" href="<?php print_unescaped(image_path('', 'favicon.png')); ?>">
 		<link rel="apple-touch-icon-precomposed" href="<?php print_unescaped(image_path('', 'favicon-touch.png')); ?>">
 		<?php foreach($_['cssfiles'] as $cssfile): ?>
